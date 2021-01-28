0ed3168413480ca9b3148ae12a49fbba3be775b4
lupe <0444735@my.scccd.edu>
2021-01-28 12:26:52 -0800

    renamed a file

--- w4/syntax_error.py
+++ w4/syntax_error.py
@@ -1,4 +1,4 @@
-
+# I picked SyntaxError. Because I got them alot when I started to learn to code. I still get them but I try to take my time and make sure my syntax is correct.
 def errorFunction(myError) #the fix to add :
     if myError != 'is this an error?':
     print('yup, there is a syntax error') #and indent this line
